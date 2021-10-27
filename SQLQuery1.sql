a9689f51dc52d017bcb7a8df991ad3e914c9e835
joirneto <joirneto@gmail.com>
2021-10-27 13:29:00 -0300

    Update .suo

---
+++ SQLQuery1.sql
@@ -0,0 +1,29 @@
+﻿USE MASTER
+
+GO
+
+CREATE DATABASE ProvaConceitoDB
+
+GO
+
+USE ProvaConceitoDB
+
+GO
+
+CREATE TABLE dbo.cliente (
+	Id uniqueidentifier default newid() not null primary key,
+	Nome varchar(100) not null,
+	Endereco varchar(100) not null
+)
+
+GO
+
+INSERT INTO dbo.cliente(Nome, Endereco)
+values
+('Joir Neto','Rua dos Curios, 371, Caoçu, Eusebio-CE'),
+('Pamela Luara','Rua C, 288, Luciano Cavalcante, Fortaleza-CE'),
+('Evaldo da Silva','Rua Rev Bolicar Pinto Bandeira, 5900, Fortaliza-Ce')
+
+GO
+
+SELECT * from cliente
