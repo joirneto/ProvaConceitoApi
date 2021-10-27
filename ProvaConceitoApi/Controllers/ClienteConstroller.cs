8f08ddefbfb305a99ff4fbdefaa44ff935e1abe4
joirneto <joirneto@gmail.com>
2021-10-27 11:27:46 -0300

    Initial commit

---
+++ ProvaConceitoApi/Controllers/ClienteConstroller.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProvaConceitoApi.Models;
+using System.Data.SqlClient;
+using ProvaConceitoApi.infra;
+using Microsoft.Extensions.Options;
+using Dapper;
+
+namespace ProvaConceitoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        private readonly IOptions<ConnectionString> connectionString;
+
+        public ClienteController(IOptions<ConnectionString> connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ClienteModel>>> Get()
+        {
+            var sql = "SELECT * FROM DBO.Cliente";
+            IEnumerable<ClienteModel> result;
+
+            using (var conn = new SqlConnection(connectionString.Value.ProvaConceito))
+            {
+                result = await conn.QueryAsync<ClienteModel>(sql);
+            }
+            return Ok(result);
+        }
+    }
+}
+
---
+++ ProvaConceitoApi/infra/Connectionstring.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProvaConceitoApi.infra
+{
+    public class ConnectionString
+    {
+        public string ProvaConceito { get; set; }
+    }
+}
+
